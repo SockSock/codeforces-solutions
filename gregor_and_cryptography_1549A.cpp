03282cfd1fbcae0a42e0c19947f2ff65176a987f
SockSock <36547748+SockSock@users.noreply.github.com>
2024-05-28 18:00:10 +0100

    Add files via upload

---
+++ gregor_and_cryptography_1549A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    for (int i = 0; i < t; ++i) {
+        int P;
+        cin >> P;
+
+        cout << 2 << " " << P - 1 << '\n';
+    }
+
+    return 0;
+}
